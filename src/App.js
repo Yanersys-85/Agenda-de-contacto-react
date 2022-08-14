5a5eb5fd59d9146a6764845b4fa6c506533e5ee8
yanersys <yanersis48@gmail.com>
2022-08-14 17:02:02 -0300

    criando codigo en app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,118 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from "reactstrap";
+import { FaRegThumbsUp, FaUserPlus, FaFacebook, FaGithub, FaRegTrashAlt, FaRegThumbsDown} from 'react-icons/fa';
+import Contatos from './components/contatos';
+import Relogio from './components/Relogio';
+
+const data = [ 
+  {id: 1, nome: "Eliecer Franco", telefone: "20198682", Correio: "prueba@gmail.com"},
+  {id: 2, nome: "Leonardo Castillo", telefone: "20198773", Correio: "prueba@gmail.com"},
+  {id: 3, nome: "Yanersys Jimenez" , telefone: "20198625", Correio: "prueba@gmail.com"},
+];
+
+class App extends React.Component {
+  state = {
+    data: data,
+    form: {
+      id: "",
+      nome: "",
+      telefone: "",
+      Correio: ""
+    }, 
+    dataSearch: [],
+    modalInsertar: false,
+    modalActualizar: false,
+    modalConfirmacion: false
+  };
+
+  handleChange = e =>{
+    this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+ 
+  //#region metodos de modales
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
+
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
+
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
+
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
+
+  mostrarConfirmacion = (dato) => {
+    this.setState({
+      form: dato,
+      modalConfirmacion: true
+    });
+    
+  };
+
+  cerrarConfirmacion = () =>{
+    this.setState({ modalConfirmacion: false });
+  }
+//#endregion
+  
+  //#region CRUD
+  insertar = () =>{
+    let valorNuevo = this.state.form;
+    valorNuevo.id = this.state.data.length + 1;
+    let lista = this.state.data;
+    lista.push(valorNuevo)
+    this.setState({data: lista, modalInsertar: false})
+  }
+  
+  editar = (dato) =>{
+    let contador = 0;
+    let lista = this.state.data;
+    lista.map((registro)=>{
+      if(dato.id === registro.id){
+        lista[contador].nombre = dato.nome;
+        lista[contador].telefono = dato.telefone;
+        lista[contador].Correio = dato.Correio;
+      }
+      contador++;
+    });
+    this.setState({data: lista, modalActualizar: false})
+  }
+
+  eliminar = (dato) =>{
+    let contador = 0; 
+    let lista = this.state.data;
+    lista.map((registro) => { 
+        if(registro.id === dato.id){
+          lista.splice(contador, 1);
+        }
+         contador++;
+      });
+      this.setState({data: lista, modalConfirmacion: false});
+  }
+
+  filtrar = (e) =>{
+    const { value } = e.target;
+    let lista = this.state.data;
+    const filtered = lista.filter(fltr => fltr.nome.toLowerCase().includes(value.toLowerCase()));
+    
+    // this.setState({data: filtered});
+    this.setState({ dataSearch: !value ?  [] : filtered});   
+  }
+
 
